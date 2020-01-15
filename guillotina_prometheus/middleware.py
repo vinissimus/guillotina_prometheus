9faffd0b420b46d290de9f92c41d2410d17cc0d2
Jordi Masip <jordi@masip.cat>
2020-01-15 19:30:11 +0100

    G6 support

--- guillotina_prometheus/middleware.py
+++ guillotina_prometheus/middleware.py
@@ -1,46 +1,44 @@
 from guillotina.utils import get_dotted_name
+from guillotina import task_vars
 from guillotina_prometheus import metrics
 from guillotina.interfaces import IApplication
 from guillotina.interfaces import IDatabase
 from guillotina.component import get_utility
 
 
-class Handler:
+class PrometheusMiddleware:
 
-    def __init__(self, app, handler):
-        self.app = app
-        self.handler = handler
+    def __init__(self, app):
+        self.next_app = app
         self.root = get_utility(IApplication, name="root")
 
-    async def __call__(self, request):
-        try:
-            try:
-                view_name = get_dotted_name(request.found_view.view_func)
-            except AttributeError:
-                view_name = get_dotted_name(request.found_view)
-        except AttributeError:
-            view_name = 'unknown'
-
+    async def __call__(self, scope, receive, send):
         for _, db in self.root:
             if IDatabase.providedBy(db):
                 pool = db.storage.pool
                 metrics.g_pg_conn_total.labels(db=db.id).set(len(pool._holders))
                 metrics.g_pg_conn_avail.labels(db=db.id).set(pool._queue.qsize())
 
-        resp = await self.handler(request)
-
-        metric = metrics.request_summary.labels(
-            method=request.method,
-            view=view_name,
-            response_code=resp.status)
-
+        resp = None
         try:
-            metric.observe(request.events['finish'] - request.events['start'])
-        except (AttributeError, KeyError):
-            pass
+            resp = await self.next_app(scope, receive, send)
+        finally:
+            request = task_vars.request.get()
+            try:
+                try:
+                    view_name = get_dotted_name(request.found_view.view_func)
+                except AttributeError:
+                    view_name = get_dotted_name(request.found_view)
+            except AttributeError:
+                view_name = 'unknown'
 
+            metric = metrics.request_summary.labels(
+                method=request.method,
+                view=view_name,
+                response_code=resp.status if resp else 500)
+
+            try:
+                metric.observe(request.events['finish'] - request.events['start'])
+            except (AttributeError, KeyError):
+                pass
         return resp
-
-
-async def middleware_factory(app, handler):
-    return Handler(app, handler)
